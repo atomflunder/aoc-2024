73000bb6ed988c2c717d6215ff9a6398b624a89c
atomflunder <80397293+atomflunder@users.noreply.github.com>
2024-12-02 16:24:28 +0100

    Day 02

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -2,9 +2,63 @@ import { readFileSync } from "fs";
 
 const file = readFileSync("./src/day-02/input.txt", "utf-8");
 
-function partOne(): void {}
+const reports: string[] = file.split("\n");
+const levels: number[][] = reports.map((r) =>
+    r.split(" ").map((l) => Number(l))
+);
 
-function partTwo(): void {}
+function isValidDecreasing(levels: number[]) {
+    for (let i = 0; i < levels.length - 1; i++) {
+        const currentLevel = levels[i];
+        const nextLevel = levels[i + 1];
+
+        const diff = currentLevel - nextLevel;
+
+        if (diff > 3 || diff < 1) return false;
+    }
+
+    return true;
+}
+
+function isValidIncreasing(levels: number[]) {
+    for (let i = 0; i < levels.length - 1; i++) {
+        const currentLevel = levels[i];
+        const nextLevel = levels[i + 1];
+
+        const diff = nextLevel - currentLevel;
+
+        if (diff > 3 || diff < 1) return false;
+    }
+
+    return true;
+}
+
+function isValid(levels: number[]) {
+    return isValidDecreasing(levels) || isValidIncreasing(levels);
+}
+
+function isValidDampened(levels: number[]): boolean {
+    if (isValid(levels)) return true;
+
+    for (let i = 0; i < levels.length; i++) {
+        // Copies the array
+        const newLevels = levels.slice();
+        newLevels.splice(i, 1);
+
+        if (isValid(newLevels)) return true;
+    }
+
+    return false;
+}
+
+function partOne(): void {
+    console.log(levels.filter((l) => isValid(l)).length);
+}
+
+function partTwo(): void {
+    console.log(levels.filter((l) => isValidDampened(l)).length);
+}
 
 partOne();
 partTwo();
+
